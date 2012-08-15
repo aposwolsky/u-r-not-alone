f116d7c0ab35899cd53f15609d25ca09fa9088de
Adam Poswolsky <adamp@foursquare.com>
2012-08-15 14:02:04 -0700

    WIP

--- config.py
+++ config.py
@@ -7,22 +7,22 @@ CONFIG = {
   # The server name for local_dev mode. Make sure the port matches what you use.
   'local_server': 'http://localhost:8080',
   # Server name for your deployed AppEngine instance
-  'prod_server': 'https://u-r-not-alone.appspot.com',
+  'prod_server': 'http://u-r-not-alone.appspot.com',
   # OAuth client ID. Must match what you set at https://foursquare.com/oauth.
   'client_id': 'N1FUZI5I2JLLMMYWILQYAG0DN5DQGPXPRY3BCFJNF0WSXOR0',
   # OAuth callback/redirect URI. Must match what you set at https://foursquare.com/oauth.
-  'redirect_uri': '%s/oauth', # (server)
+  'redirect_uri': 'http://u-r-not-alone.appspot.com/oauth', # (server)
   # Format string to serve URL content out of. Not necessarily required.
   'content_uri': '%s/content?content_id=%s', # (server, content_id)
   # The foursquare API version string to pass. See: https://developer.foursquare.com/overview/versioning
   'api_version': '20120608',
   # A name for the hompage and titles.
-  'site_name': '',
+  'site_name': 'U R Not Alone',
   # A description for the home page
-  'site_description': '',
+  'site_description': 'We know who you were with last summer',
   # If true, we use local_server, and log actions instead of POSTing to foursquare
   # Be sure to set this to false when you actually want to deploy.
-  'local_dev': True,
+  'local_dev': False,
   # AppEngine debug mode
   'debug': True,
   # These can either be a path (on this server), or an external URI
