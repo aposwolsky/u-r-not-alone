859c3dd4d885c6b38c8d70313e8593797eac2d6a
Adam Poswolsky <adamp@foursquare.com>
2012-08-16 16:31:15 -0700

    Updated secrets

--- foursquare_secrets.py
+++
@@ -1,8 +0,0 @@
-# Copy this file to 'foursquare_secrets.py' FIRST, then fill it out.
-
-SECRETS = {
-  # OAuth client secret. Must match what you set at https://foursquare.com/oauth.
-  'client_secret': 'WBNRJBDBP1AFNNGBF1KIIM1JC5UJDY34BOWVKVTJV0ITRPL3',
-  # Foursquare push API secret. Must match what you set at https://foursquare.com/oauth.
-  'push_secret': 'CUJYIYZJMQTVK1ECCNCPYRNXXFGUMULLHOTKCDQ3QBNFVJTX',
-}
